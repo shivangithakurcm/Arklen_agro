01a0150c0fef9a8a3bf90f520053c9511b102848
Shreyansh <shreyanshsahu.cm@gmail.com>
2026-06-10 10:21:05 +0530

    added migration for parent_id and removed a class name

---
+++ database/migrations/2026_06_10_044916_update_member_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('parent_id')->nullable()->after('sponsor_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('parent_id');
+        });
+    }
+};
